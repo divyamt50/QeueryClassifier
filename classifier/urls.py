7d12686f29d04742abd03f0f35c58b486c53df63
divyamt <divyamt50@gmail.com>
2025-02-06 00:37:22 +0530

    changed urls

--- classifier/urls.py
+++ classifier/urls.py
@@ -1,6 +1,7 @@
+# classifier/urls.py
 from django.urls import path
 from .views import classify_query
 
 urlpatterns = [
-    path("classify/", classify_query, name="classify_query"),
+    path("classify/", classify_query, name="classify_query"),  # This will handle classification requests
 ]
--- queryclassifier/urls.py
+++ queryclassifier/urls.py
@@ -1,9 +1,10 @@
+# queryclassifier/urls.py
 from django.contrib import admin
 from django.urls import path, include
 from classifier.views import home
 
 urlpatterns = [
-    path("", home),
+    path("", home, name="home"),  # This is for the home page
     path("admin/", admin.site.urls),
-    path("api/", include("classifier.urls")),  # Include classifier API
+    path("api/", include("classifier.urls")),  # Include classifier URLs for the API
 ]
